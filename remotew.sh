bfa9c214f720918f4ec161a3e757d5807c5c78ba
dantehemerson <dantehemerson@gmail.com>
2018-09-30 09:44:48 -0500

    Create functions for get date

--- remotew.sh
+++ remotew.sh
@@ -4,14 +4,37 @@ echo $1
 
 TODAY=$(date +%FT%T)
 
-TODAY_YEAR=${TODAY:0:4}
-TODAY_MONTH=${TODAY:5:2}
-TODAY_DAY=${TODAY:8:2}
-TODAY_HOUR=${TODAY:11:2}
-TODAY_MINUTE=${TODAY:14:2}
-TODAY_SECOND=${TODAY:17:2}
+function getYear(){
+  echo ${1:0:4}
+}
 
-printf "Fecha separada : \n"
+function getMonth(){
+  echo ${1:5:2}
+}
+
+function getDay(){
+  echo ${1:8:2}
+}
+
+function getHour(){
+  echo ${1:11:2}
+}
+
+function getMinute(){
+  echo ${1:14:2}
+}
+
+function getSecond(){
+  echo ${1:17:2}
+}
+
+TODAY_YEAR=$(getYear $TODAY)
+TODAY_MONTH=$(getMonth $TODAY)
+TODAY_DAY=$(getDay $TODAY)
+TODAY_HOUR=$(getHour $TODAY)
+TODAY_MINUTE=$(getMinute $TODAY)
+TODAY_SECOND=$(getSecond $TODAY)
+
 printf "YEAR = ${TODAY_YEAR}\n"
 printf "MONTH = ${TODAY_MONTH}\n"
 printf "DAY = ${TODAY_DAY}\n"
