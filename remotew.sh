dc9fc23decd8873a80aac3ea3d940f6316b69631
dantehemerson <dantehemerson@gmail.com>
2018-09-30 12:26:29 -0500

    Remove not used functions

--- remotew.sh
+++ remotew.sh
@@ -1,29 +1,4 @@
 #!/bin/bash
-#GIT_AUTHOR_DATE='2017-10-01T00:01' GIT_COMMITTER_DATE='2017-10-01T00:01' git commit -m 'Graph Data 2017-10-01T00:01'
-
-function getYear(){
-  echo ${1:0:4}
-}
-
-function getMonth(){
-  echo ${1:5:2}
-}
-
-function getDay(){
-  echo ${1:8:2}
-}
-
-function getHour(){
-  echo ${1:11:2}
-}
-
-function getMinute(){
-  echo ${1:14:2}
-}
-
-function getSecond(){
-  echo ${1:17:2}
-}
 
 function help() {
 	printf 	"\t\tGIT REMOTE WORK - gitr v0.0.1\n
@@ -61,26 +36,10 @@ function commit() {
 	INIT_DATE=$(getInitialDateFromFile)
 	DIFF_DAYS=$(getDiffDays ${INIT_DATE})
 	COMMIT_DAY=$(getCommitDay ${REMOTE_DATE} ${DIFF_DAYS})
-	printf "${COMMIT_DAY}\n\n\n"
+	printf "GIT_AUTHOR_DATE='${COMMIT_DAY}' GIT_COMMITTER_DATE='${COMMIT_DAY}' git commit -m 'Your message'\n\n\n"
 }
 
 commit
-
-TODAY=$(date +%FT%T)
-TODAY_YEAR=$(getYear $TODAY)
-TODAY_MONTH=$(getMonth $TODAY)
-TODAY_DAY=$(getDay $TODAY)
-TODAY_HOUR=$(getHour $TODAY)
-TODAY_MINUTE=$(getMinute $TODAY)
-TODAY_SECOND=$(getSecond $TODAY)
-
-printf "YEAR = ${TODAY_YEAR}\n"
-printf "MONTH = ${TODAY_MONTH}\n"
-printf "DAY = ${TODAY_DAY}\n"
-printf "HOUR = ${TODAY_HOUR}\n"
-printf "MINUTE = ${TODAY_MINUTE}\n"
-printf "SECOND = ${TODAY_SECOND}\n"
-
 
 # __main__
 if [[ -n $1 ]]; then
@@ -99,4 +58,3 @@ if [[ -n $1 ]]; then
 else
 	printf "No aguments"
 fi
-
